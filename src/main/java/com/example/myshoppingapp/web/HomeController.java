b370fefcf753abfd10bc3d89dfce21ed7ff530c4
MartinMenev <martin.menew@gmail.com>
2023-01-23 08:11:11 +0200

    Added modal-box

--- src/main/java/com/example/myshoppingapp/web/HomeController.java
+++ src/main/java/com/example/myshoppingapp/web/HomeController.java
@@ -1,5 +1,7 @@
 package com.example.myshoppingapp.web;
 
+import com.example.myshoppingapp.service.PictureService;
+import com.example.myshoppingapp.service.ProductService;
 import com.example.myshoppingapp.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -10,10 +12,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
 
     private final UserService userService;
+    private final ProductService productService;
 
     @Autowired
-    public HomeController(UserService userService) {
+    public HomeController(UserService userService, PictureService productService, ProductService productService1) {
         this.userService = userService;
+        this.productService = productService1;
     }
 
     @GetMapping("/")
@@ -28,6 +32,9 @@ public class HomeController {
     @GetMapping("home")
     public String home(Model model) {
         model.addAttribute("username", userService.getLoggedInUser());
+        if (userService.getLoggedInUser() !=null) {
+            model.addAttribute("products", productService.getListedProducts());
+        }
         return "home";
     }
 }
